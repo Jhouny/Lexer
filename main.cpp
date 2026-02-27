16b110a6889463530879b58348ee3034fbcd2c6b
Jhouny <jhonyaraujo9121@gmail.com>
2026-02-27 13:17:53 +0100

    Added support for inputing expressions through terminal arguments

--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 #include "etat.h"
 #include "automate.h"
 
-int main(void) {
-   string chaine("1*(3+56)");
+int main(int argc, char* argv[]) {
+   if (argc != 2) {
+       cout << "Usage: " << argv[0] << " <expression>" << endl;
+       return 1;
+   }
+
+   string chaine = argv[1];
    
    Automate transformers;
    Lexer l(chaine);
